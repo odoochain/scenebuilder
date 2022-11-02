6d3cb28e87e784705c477f198f35a318b950347e
Oliver Löffler <22102800+oliver-loeffler@users.noreply.github.com>
2022-11-02 13:50:12 +0100

    fix: Disabled mnemonic parsing on RecentDocumentsButton instances on welcome page in oder to have correct file name rendering for filenames with underscores. (#578)
    Co-authored-by: Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>

---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module app {
+    requires java.desktop;
+    requires java.logging;
+    requires java.prefs;
+    requires javax.json;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.web;
+    requires kit;
+}
